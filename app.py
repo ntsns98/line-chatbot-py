1bed90c66482920ac58bfbfee3d0f3df13106736
AustinLiu <ntsns98@hotmail.com>
2024-10-03 18:04:24 +0800

    Update app.py
    Debug

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    message = f"What you say is '{message}'"
+    message = TextSendMessage(text=f"What you say is {event.message.text}")
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
